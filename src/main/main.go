c5ebde959cb8164f9b40d0941500addbe9f9a9b7
alexbarksdale <ale.barksdale@gmail.com>
2020-03-12 20:47:26 -0500

    Parsing JSON form request

--- src/main/main.go
+++ src/main/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 
 	"github.com/labstack/echo"
@@ -34,6 +37,70 @@ func getCats(c echo.Context) error {
 
 }
 
+type Cat struct {
+	Name string `json:"name"`
+	Type string `json:"type"`
+}
+
+func addCat(c echo.Context) error {
+	cat := Cat{}
+
+	defer c.Request().Body.Close()
+
+	body, err := ioutil.ReadAll(c.Request().Body)
+	if err != nil {
+		log.Printf("Failed reading the request body %s", err)
+		return c.String(http.StatusInternalServerError, "")
+	}
+
+	err = json.Unmarshal(body, &cat)
+	if err != nil {
+		log.Printf("Failed unmarshalling in addCats %s", err)
+		return c.String(http.StatusInternalServerError, "")
+	}
+
+	log.Printf("This is your cat: %#v", cat)
+	return c.String(http.StatusOK, "We got your cat")
+}
+
+type Dog struct {
+	Name string `json:"name"`
+	Type string `json:"type"`
+}
+
+func addDog(c echo.Context) error {
+	dog := Dog{}
+
+	defer c.Request().Body.Close()
+
+	err := json.NewDecoder(c.Request().Body).Decode(&dog)
+	if err != nil {
+		log.Printf("Failed processing addDog request %s", err)
+		return echo.NewHTTPError(http.StatusInternalServerError, "")
+	}
+
+	log.Printf("This is your dog: %#v", dog)
+	return c.String(http.StatusOK, "We got your dog")
+}
+
+type Hamster struct {
+	Name string `json:"name"`
+	Type string `json:"type"`
+}
+
+func addHamster(c echo.Context) error {
+	hamster := Hamster{}
+
+	err := c.Bind(&hamster)
+	if err != nil {
+		log.Printf("Failed processing addHamster request %s", err)
+		return echo.NewHTTPError(http.StatusInternalServerError, "")
+	}
+
+	log.Printf("This is your hamster: %#v", hamster)
+	return c.String(http.StatusOK, "We got your hamster")
+}
+
 func main() {
 	fmt.Println("GoWeb server")
 
@@ -41,6 +108,9 @@ func main() {
 
 	e.GET("/", GoWeb)
 	e.GET("/cats/:data", getCats)
+	e.POST("/cats", addCat)         // V1 - Vanilla (fastest)
+	e.POST("/dogs", addDog)         // V2 - Vanilla (fast - usually the go to)
+	e.POST("/hamsters", addHamster) // Utilize echo (slowest)
 
 	e.Start(":8000")
 }
